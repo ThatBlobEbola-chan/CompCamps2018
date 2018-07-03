83ee0754e2d4b1a56843094664d530fa6882e5d9
Garry <2003wolfboy@gmail.com>
2018-07-03 16:05:46 -0600

    updatte

---
+++ Python/main.py
@@ -0,0 +1,14 @@
+name = input("Buenos dias, (TYPE NAME HERE): ")
+
+def welcome(name):
+    if name == "jeff":
+        print("No, bad meme")
+    else:
+        print("Welcome to BITCONNNEEEEEEEEECT, {}".format(name))
+
+welcome(name)
+
+sodiebois = ["Godjavin","Ebola Granola","regret"]
+
+for sodieboi in sodiebois:
+    welcome(sodieboi)
