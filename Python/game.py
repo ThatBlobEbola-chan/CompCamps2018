bb61abec11ad5914bdb2577753c6a76eb34a8ea5
Garry <2003wolfboy@gmail.com>
2018-07-04 10:23:36 -0600

    .

--- Python/game.py
+++ Python/game.py
@@ -1,17 +1,23 @@
-import os, pygame
+import os, pygame, math, random
 
 from settings import Settings
+from godjavin import godjavin
 
 class Game():
 	def __init__(self):
 		self.background = pygame.image.load("images/bliss.jpg")
-
+		self.enemy = None
 		self.godjavins = [
-			godjavin("GodJavin")
+			godjavin("GodJavin"),
+			godjavin("Test")
 		]
+		random.shuffle(self.godjavins)
 
 	def loop(self, screen):
 		clock = pygame.time.Clock()
+
+		if not self.enemy:
+			self.enemy = self.godjavins.pop()
 
 		while True:
 			delta_t = clock.tick(Settings.frameRate)
@@ -22,6 +28,8 @@ class Game():
 
 			screen.fill((0, 0, 0))
 			screen.blit(self.background, (0,0))
+
+			screen.blit(self.enemy.img, (Settings.width - 800, 0))
 
 			pygame.display.flip()
 
--- Python/godjavin.py
+++ Python/godjavin.py
@@ -1,4 +1,8 @@
+import pygame
+
 class godjavin:
     def __init__(self, name):
         self.name = name
-        self.image = "images/" + self.name.lower() + ".jpg"
+        self.image = "images/" + self.name.lower() + ".png"
+        self.img = pygame.image.load(self.image)
+        self.img = pygame.transform.scale(self.img, (800, 600))
